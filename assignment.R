c9ecf4df93edcc1492d37495ef8a0da7bbaa534b
EchoYue <echoxiongyue1124@gmail.com>
2022-06-24 14:32:49 +0200

    update code and delete template

--- assignment.R
+++ assignment.R
@@ -1,16 +1,17 @@
 # install  the required R packages
 devtools::install_github("CaroHaensch/CTIS")
+install.packages("readr")
 library(CTIS)
+library(readr)
 
+# read in the original data
+ori_data_0802 <- read_csv("./ori_data/2020-08-02_full.csv")
+ori_data_0803 <- read_csv("./ori_data/2020-08-03_full.csv")
+ori_data_0804 <- read_csv("./ori_data/2020-08-04_full.csv")
+ori_data_0805 <- read_csv("./ori_data/2020-08-05_full.csv")
+ori_data_0806 <- read_csv("./ori_data/2020-08-06_full.csv")
+ori_data_0807 <- read_csv("./ori_data/2020-08-07_full.csv")
+ori_data_0808 <- read_csv("./ori_data/2020-08-08_full.csv")
 
-ori_data <- CTIS_open_data_country(indicator = "vaccine_acpt", 
-                                   type = "daily", 
-                                   country = "Germany",
-                                   daterange = "20210501-20210503")
-
-username <- "yue.xiong@stat.uni-muenchen.de"
-password <- "967270Xgy@"
-
-ori_data_0802 <- CTIS_microdata(username = username, 
-                           password = password,
-                           date = "2020-08-02")
+head(ori_data_0802)
+ori_data_0802 
