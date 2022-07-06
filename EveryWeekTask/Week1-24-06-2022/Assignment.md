9a43bd6b233e59f2a9ae957ef33402e4ffbea908
EchoYue <echoxiongyue1124@gmail.com>
2022-07-06 16:19:06 +0200

    update assignment

--- EveryWeekTask/Week1-24-06-2022/Assignment.md
+++ EveryWeekTask/Week1-24-06-2022/Assignment.md
@@ -4,5 +4,7 @@
 
 - Task 1: plot the distribution of the original data and the synthetic data for general comparison.
 
+With one-way utility measure, we try to plot the compare histogram plots included in package *synthpop* to have a general overview of the synthetic dataset and the original dataset. With too many variables involved in both datasets, we also manage to subset 5 of the variables from the original columns and show you the comparison analysis done by the __compare__ function. To be more specific, the 5 variables are selected randomly, i.e., B3 and B1_1 from the "symptoms" group, 
+
 
 - Task 2: evaluate the propensity mean squared error from the synthetic data to the original data (hint: use the utility.gen function implemented)
--- assignment.R
+++ assignment.R
@@ -29,11 +29,6 @@ colnames(syn_data)[colnames(syn_data) == "sample_weight"] <- "weight"
 # columns to be included from the original dataset
 cols_list <- colnames(syn_data)
 
-# datentest <- vroom(list.files(pattern = "*_full.csv$")[1],
-#                               show_col_types = FALSE) %>%
-#              select(all_of(cols_list))
-# dim(datentest)
-
 # initialize an empty dataset list
 ori_dataset <- list()
 
@@ -48,8 +43,9 @@ dim(ori_dataset[[2]])[2] == dim(syn_data)[2]
 
 # bind the original datasets from 0802 to 0808 vertically
 bindori_dataset <- bind_rows(ori_dataset)
-bindori_dataset <- data.frame(bindori_dataset)
+bindori_dataset <- as.data.frame(bindori_dataset)
 dim(bindori_dataset)
+dim(syn_data)
 
 ####################### Step2: Evaluating the utility of the syn data #######################
 # In step2, we try to evaluate the utility of the synthetic dataset with one-way marginal
@@ -57,33 +53,29 @@ dim(bindori_dataset)
 # the compare plots and pMSE/S_pMSE. (2). And for the two-way utility, the synthetic dataset is
 # evaluated with the utility tables which takes up a heatmap fashion/manner.
 
-# subset some example columns and try plotting the histograms
+# subset some example columns and try plotting the compare histograms
+# these are symptoms variables
 symptoms <- c("B3","B4","B1_1","B1_2","B1_3","B1_4","B1_5","B1_6","B1_7",
               "B1_8","B1_9","B1_10","B1_12","B1_13",
               "B1b_x1","B1b_x2","B1b_x3","B1b_x4","B1b_x5","B1b_x6","B1b_x7",
               "B1b_x8","B1b_x9","B1b_x10","B1b_x12","B1b_x13","B2")
 
+# these are testing variables
+testing <- c("B7")
 
-testing <- c("B0","B7","B8a",
-             "B15_1","B15_2",
-             "B15_3","B15_4",
-             "B15_5","B15_6","B15_7")
+# Columns `B0`, `B8a`, `B15_1`, `B15_2`, `B15_3`, etc. don't exist
 
-testori_dataset <- bindori_dataset[, symptoms_1]
-testori_dataset <- data.frame(testori_dataset)
-testsyn_dataset <- data.frame(syn_data[, symptoms_1])
+ori_dataset_symptoms <- as.data.frame(bindori_dataset[, symptoms])
+syn_dataset_symptoms <- as.data.frame(syn_data[, symptoms])
+
+ori_dataset_testing <- as.data.frame(bindori_dataset$B7)
+syn_dataset_testing <- as.data.frame(syn_data$B7)
 
 ### (1). one-way marginals using compare()
-synxxx <- syn.try.passive
 
-typeof(bindori_dataset$B3)
-head(bindori_dataset$B3, n=10)
-compare_symptoms <- compare(data = as.factor(bindori_dataset$B3),
-                            object = as.factor(syn_data$B3))
-
-## for var B3
-compare(object = data.frame(B3 = testsyn_dataset$B3), 
-        data = data.frame(B3 = testori_dataset$B3), 
+## for var B3 -> symptoms
+compare(object = data.frame(B3 = syn_dataset_symptoms$B3),
+        data = data.frame(B3 = ori_dataset_symptoms$B3),
         vars = c("B3"), cont.na = NULL,
         msel = NULL, stat = "percents", breaks = 20,
         nrow = 2, ncol = 2, rel.size.x = 1,
@@ -91,36 +83,57 @@ compare(object = data.frame(B3 = testsyn_dataset$B3),
         cols = c("#1A3C5A","#4187BF"),
         plot = TRUE, table = TRUE)
 
-## for var B4
-length(testsyn_dataset$B3)
-length(testori_dataset$B3)
-length(testsyn_dataset$B4)
-length(testori_dataset$B4)
-
-compare(object = data.frame(B4 = testsyn_dataset$B4), 
-        data = data.frame(B4 = testori_dataset$B4), 
-        vars = "B4", cont.na = NULL,
+## for var B1_1 -> symptoms
+compare(object = data.frame(B1_1 = syn_dataset_symptoms$B1_1),
+        data = data.frame(B1_1 = ori_dataset_symptoms$B1_1),
+        vars = "B1_1", cont.na = NULL,
         msel = NULL, stat = "percents", breaks = 20,
         nrow = 2, ncol = 2, rel.size.x = 1,
         utility.stats = c("pMSE", "S_pMSE"),
         cols = c("#1A3C5A","#4187BF"),
         plot = TRUE, table = FALSE)
 
-head(testori_dataset)
-# plot the distribution of the synthetic dataset
-var_testori_dataset <- melt(testori_dataset)  # break down into var variables
+## for var B7 -> testing
+compare(object = data.frame(B7 = syn_data$B7),
+        data = data.frame(B7 = bindori_dataset$B7),
+        vars = "B7", cont.na = NULL,
+        msel = NULL, stat = "percents", breaks = 20,
+        nrow = 2, ncol = 2, rel.size.x = 1,
+        utility.stats = c("pMSE", "S_pMSE"),
+        cols = c("#1A3C5A","#4187BF"),
+        plot = TRUE, table = FALSE)
 
+## for var D1 -> module B
+compare(object = data.frame(D1 = syn_data$D1),
+        data = data.frame(D1 = bindori_dataset$D1),
+        vars = "D1", cont.na = NULL,
+        msel = NULL, stat = "percents", breaks = 20,
+        nrow = 2, ncol = 2, rel.size.x = 1,
+        utility.stats = c("pMSE", "S_pMSE"),
+        cols = c("#1A3C5A","#4187BF"),
+        plot = TRUE, table = FALSE)
 
-var_testori_dataset$variable
+## for var D2 -> module B
+compare(object = data.frame(D2 = syn_data$D2),
+        data = data.frame(D2 = bindori_dataset$D2),
+        vars = "D2", cont.na = NULL,
+        msel = NULL, stat = "percents", breaks = 20,
+        nrow = 2, ncol = 2, rel.size.x = 1,
+        utility.stats = c("pMSE", "S_pMSE"),
+        cols = c("#1A3C5A","#4187BF"),
+        plot = TRUE, table = FALSE)
 
-ggplot(var_testori_dataset, aes(x=value, fill=variable)) +
-       geom_histogram()+
-       facet_grid(variable~.)
-
+compare(object = data.frame(B7 = syn_data$B7),
+        data = data.frame(B7 = bindori_dataset$B7),
+        vars = "B7", cont.na = NULL,
+        msel = NULL, stat = "percents", breaks = 20,
+        nrow = 2, ncol = 2, rel.size.x = 1,
+        utility.stats = c("pMSE", "S_pMSE"),
+        cols = c("#1A3C5A","#4187BF"),
+        plot = TRUE, table = FALSE)
 
 utility.gen(object = syn_data,
             data = bindori_dataset,
             not.synthesised = NULL,
             cont.na = NULL,
             print.stats = c("pMSE", "S_pMSE"))  # do not know why the s_pMSE is quite large
- 
