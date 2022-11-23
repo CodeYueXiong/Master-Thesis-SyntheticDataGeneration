bdfa35d619ac8a0861239b82ea868872e044ad6b
EchoYue <echoxiongyue1124@gmail.com>
2022-11-23 14:51:52 +0100

    update code

---
+++ SyntheticData/Yue/data_preprocess.R
@@ -0,0 +1,12 @@
+########  ----- source code for data preprocessing -----
+
+# load the required packages
+library(readr)
+library(vroom)
+library(tidyverse)
+library(arsenal)
+library(reshape2)
+library(synthpop)
+library(ggplot2)
+library(dbplyr)
+
